837f4f9f2833d76e71f6c0bb8a1e9bff6c73d0c4
Dave Lindqvist <lindqvist.dave@gmail.com>
2024-06-07 13:17:17 -0700

    Remove unnecessary imported function

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
