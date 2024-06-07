656a134e59ca8f1bed55f30186b5cc1a1833bf73
Dave Lindqvist <lindqvist.dave@gmail.com>
2024-06-07 10:51:56 -0700

    Prepare endpoints

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,6 @@ import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
-import { bookAppointment } from './clients/bookAppointment';
 import { pgCron } from './helpers/pgCron';
 
 const pool = new Pool({
@@ -28,14 +27,35 @@ dotenv.config();
 
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World');
-});
+// Confirms client of their reservation
+app.get(
+  '/appointments/:clientId',
+  (req: Request, res: Response, next: NextFunction) => {},
+);
 
-app.post('/appointment', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World');
-  bookAppointment(pool, 1, 1);
-});
+// Client retrieves available provider slots
+app.get(
+  '/providers/:providerId/appointments',
+  (req: Request, res: Response, next: NextFunction) => {},
+);
+
+// Submit provider appointments in 15-minute intervals
+app.post(
+  '/providers/:providerId/appointments',
+  (req: Request, res: Response, next: NextFunction) => {},
+);
+
+// Viewing a specific appointment which will lock it in database (technically not idempotent)
+app.get(
+  '/clients/:clientId/appointments/:appointmentId',
+  (req: Request, res: Response, next: NextFunction) => {},
+);
+
+// Reserve appointment after viewing
+app.patch(
+  '/clients/:clientId/appointments/:appointmentId',
+  (req: Request, res: Response, next: NextFunction) => {},
+);
 
 app.listen(8000, () => {
   console.log(`server running on port ${process.env.PORT}`);
